46fcdb8097d1a50103eaa9b28faa4a3c666a7d72
Your Name <you@example.com>
2015-08-23 12:49:31 +0600

    init

---
+++ trunk/src/org/zzz/FirstTest.java
@@ -0,0 +1,17 @@
+package org.zzz;
+
+/**
+ * This is the test class
+ * 
+ * @author zhangxh
+ */
+public class FirstTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Google World!");
+    }
+
+}
